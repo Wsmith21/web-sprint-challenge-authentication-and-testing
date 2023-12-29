88d49eda3c55eba4784f7f7a431a202ef5159290
walyn smithe <walynsmithe@yahooe.com>
2023-12-29 12:39:24 -0600

    updated

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,11 +2,9 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
-
-
 const router = express.Router();
 
+// Placeholder for users (static array acting as persistent storage)
 const users = [];
 
 // Endpoint for user registration
@@ -18,35 +16,33 @@ router.post('/register', async (req, res) => {
   }
 
   try {
-    // Check if the username already exists in the mock array (you would typically query a database here)
+    // Check if the username already exists in the users array
     const existingUser = users.find(user => user.username === username);
 
     if (existingUser) {
       return res.status(400).json({ message: 'Username already exists' });
     }
 
-    // Create a new user object
+    // Hash the password before storing it (in a real app, store in a database hashed)
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user object to save (in a real app, save to a database)
     const newUser = {
       username,
-      password, // Note: In a real application, passwords should be hashed before storing
+      password: hashedPassword,
     };
 
-    // Add the new user to the mock array (you would typically insert into a database here)
+    // Push the new user to the users array (in a real app, save to a database)
     users.push(newUser);
 
-    // Return the user details in the response upon successful registration
+    // Return user details upon successful registration
     return res.status(201).json({ id: users.length, username: newUser.username });
   } catch (error) {
-    return res.status(500).json({ message: 'Error registering user' });
+    return res.status(500).json({ message: 'Error creating user' });
   }
 });
 
-
-
-
-
-
-
+// Endpoint for user login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -54,30 +50,32 @@ router.post('/login', async (req, res) => {
     return res.status(400).json({ message: 'Username and password required' });
   }
 
+  // Find user by username in the users array
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
   try {
-    const user = await userService.findByUsername(username);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
+    // Compare provided password with user's hashed password
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ token: token });
+      return res.status(400).json({ message: 'Invalid credentials' });
     }
 
+    // Create a JWT token
     const token = jwt.sign({ username: user.username }, 'your_secret_key', { expiresIn: '1h' });
 
+    // Respond with welcome message and token upon successful login
     res.json({
       message: `Welcome, ${user.username}`,
       token: token,
     });
   } catch (error) {
-    console.error(error);
-    res.status(200).json({ message: 'invalid credentials' });
-  } 
+    res.status(500).json({ message: 'Login failed' });
+  }
 });
-
 
 module.exports = router;
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,19 +1,24 @@
-// ./middleware/restricted.js
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  if (req.baseUrl === '/api/jokes') {
+    const token = req.headers.authorization;
 
-  if (!token || !token.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
+    if (!token) {
+      return res.status(401).json({ message: 'Token required' });
+    }
 
-  const formattedToken = token.split(' ')[1]; // Extract the token value
-  try {
-    const decodedToken = jwt.verify(formattedToken, 'your_secret_key');
-    req.decodedToken = decodedToken; // Attach the decoded token to the request object
-    next(); // Proceed to the next middleware or endpoint
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
+    // Verify the token here using jwt.verify for validation
+    jwt.verify(token, 'your_secret_key', (err, decodedToken) => {
+      if (err) {
+        return res.status(403).json({ message: 'Token invalid' });
+      }
+
+      // Token is valid, proceed to the jokes route
+      next();
+    });
+  } else {
+    // For other routes, proceed to the next middleware
+    next();
   }
 };
