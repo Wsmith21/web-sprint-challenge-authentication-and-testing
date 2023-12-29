d8591158d896803a779d24bea78309d5bad0c7a1
walyn smithe <walynsmithe@yahooe.com>
2023-12-28 18:28:00 -0600

    update

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const knex = require('./testing/knexfile.js'); // Replace with your Knex configuration
+const knex = require('../web-sprint-challenge-authentication-and-testing/knexfile.js'); // Replace with your Knex configuration
 
 const router = express.Router();
 
