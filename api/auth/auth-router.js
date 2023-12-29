e23d95bba4b324b62e66285fa004e7c6a17cc381
walyn smithe <walynsmithe@yahooe.com>
2023-12-29 17:28:50 -0600

    updated

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const knex = require('knex')(require('./knexfile.js'));
+const knex = require('knex');
 
 const router = express.Router();
 
