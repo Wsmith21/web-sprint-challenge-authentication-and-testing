5b3e5226dc4d22322e01b901c45acd5e76aa61a5
walyn smithe <walynsmithe@yahooe.com>
2023-12-28 18:49:24 -0600

    updated

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,87 +1,80 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const knex = require('knex')
+const knex = require('knex'); // Import the User model that interacts with the database
+
+
+
 
 const router = express.Router();
 
+// Replace this with your actual secret key
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
+
 // POST /api/auth/register
 router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
   try {
-    const { username, password } = req.body;
-
-    // Check if username or password is missing
-    if (!username || !password) {
-      return res.status(400).json({ message: 'username and password required' });
-    }
-
-    // Check if username already exists in the database
+    // Check if the username already exists in the database
     const existingUser = await knex('users').where({ username }).first();
+
     if (existingUser) {
-      return res.status(400).json({ message: 'username taken' });
+      return res.status(400).json({ message: 'Username already exists' });
     }
 
-    // Hash the password before saving it to the database
-    const hashedPassword = await bcrypt.hash(password, 8); // Maximum 2^8 rounds of hashing
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10); // Use an appropriate bcrypt hash value
 
-    // Insert the new user into the database
-    const newUser = await knex('users').insert({ username, password: hashedPassword });
+    // Create a new user object to save in the database
+    await knex('users').insert({ username, password: hashedPassword });
 
-    res.status(201).json({ id: newUser[0], username, password: hashedPassword });
+    // Return the user details in the response upon successful registration
+    return res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: 'Error creating user' });
   }
 });
 
-module.exports = router;
-
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
-
+// POST /api/auth/login
 router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password required' });
+  }
+
   try {
-    const { username, password } = req.body;
-
-    // Check if username or password is missing
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Username and password required' });
-    }
-
-    // Check if the username exists in the database
+    // Find the user by username in the database
     const user = await knex('users').where({ username }).first();
+
     if (!user) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
     // Compare the provided password with the hashed password in the database
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    // If the username and password are correct, generate a JWT token
+    // Create a JWT token
     const token = jwt.sign({ username: user.username }, JWT_SECRET, { expiresIn: '1h' });
 
-    res.status(200).json({ message: `Welcome, ${user.username}`, token });
+    res.json({
+      message: `Welcome, ${user.username}`,
+      token: token,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Login failed' });
   }
 });
-
-
-
-function buildToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-    role: user.role,
-  };
-  const options = {
-    expiresIn: '1d',
-  };
-  return jwt.sign(payload, JWT_SECRET, options);
-}
-
 
 module.exports = router;
